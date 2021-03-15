31e874f5a6a9427e5c77ef8a47f6193456491527
omaritgis <omartotangy@gmail.com>
2021-03-15 19:41:09 +0100

    Init c

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeUsefulStuffInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
